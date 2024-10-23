058edd29b050a9cbf1cbf7b8ec4f87dfaf8c9280
sam neumann <sam.neumann2@gmail.com>
2024-10-22 18:52:34 -0700

    Added refresh device list function

--- MauiUSB/MainPage.xaml.cs
+++ MauiUSB/MainPage.xaml.cs
@@ -1,8 +1,15 @@
-﻿namespace MauiUSB
+﻿using FTD2XX_NET;
+
+namespace MauiUSB
+
 {
     public partial class MainPage : ContentPage
     {
-        
+        UInt32 ftdiDeviceCount = 0;
+        FTDI.FT_STATUS ftStatus = FTDI.FT_STATUS.FT_OK;
+
+        // Allocate storage for device info list
+        FTDI.FT_DEVICE_INFO_NODE[] ftdiDeviceList = new FTDI.FT_DEVICE_INFO_NODE[64];
 
         public MainPage()
         {
@@ -11,7 +18,35 @@
 
         private void BtnOpenClose_Clicked(object sender, EventArgs e)
         {
+            RefreshDeviceList();
+        }
 
+        private void RefreshDeviceList()
+        {
+            // Determine the number of FTDI devices connected to the machine
+            ftStatus = myFtdiDevice.GetNumberOfDevices(ref ftdiDeviceCount);
+            // Check status
+            if (ftStatus == FTDI.FT_STATUS.FT_OK)
+            {
+                Console.WriteLine("Number of FTDI devices: " + ftdiDeviceCount.ToString());
+                Console.WriteLine("");
+            }
+            else
+            {
+                // Wait for a key press
+                Console.WriteLine("Failed to get number of devices (error " + ftStatus.ToString() + ")");
+                Console.ReadKey();
+                return;
+            }
+
+            // If no devices available, return
+            if (ftdiDeviceCount == 0)
+            {
+                // Wait for a key press
+                Console.WriteLine("Failed to get number of devices (error " + ftStatus.ToString() + ")");
+                Console.ReadKey();
+                return;
+            }
         }
 
         private void BtnClear_Clicked(object sender, EventArgs e)
